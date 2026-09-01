acf273cd516f21e67beb14d8ca7e566da7a8acb3
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-01 22:12:23 +0700

    feat: add birth date to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
  * @property string $name
  * @property string $email
  * @property string|null $phone
+ * @property Carbon|null $birth_date
  * @property UserRole $role
  * @property bool $is_active
  * @property string|null $avatar
@@ -27,7 +28,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
  * @property Carbon|null $email_verified_at
  * @property string $password
  */
-#[Fillable(['name', 'email', 'phone', 'password', 'role', 'is_active', 'avatar', 'google_id', 'current_team_id'])]
+#[Fillable(['name', 'email', 'phone', 'birth_date', 'password', 'role', 'is_active', 'avatar', 'google_id', 'current_team_id'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable implements PasskeyUser
 {
@@ -38,6 +39,7 @@ class User extends Authenticatable implements PasskeyUser
     {
         return [
             'email_verified_at' => 'datetime',
+            'birth_date' => 'date',
             'password' => 'hashed',
             'role' => UserRole::class,
             'is_active' => 'boolean',

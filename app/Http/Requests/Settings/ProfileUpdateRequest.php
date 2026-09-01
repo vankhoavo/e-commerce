a9fe5f3fc986e0868d16ae1624f9bea23a7ccf37
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-01 22:12:34 +0700

    feat: update profile validation

--- app/Http/Requests/Settings/ProfileUpdateRequest.php
+++ app/Http/Requests/Settings/ProfileUpdateRequest.php
@@ -10,11 +10,7 @@ class ProfileUpdateRequest extends FormRequest
 {
     use ProfileValidationRules;
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
+    /** @return array<string, ValidationRule|array<mixed>|string> */
     public function rules(): array
     {
         return $this->profileRules($this->user()->id);

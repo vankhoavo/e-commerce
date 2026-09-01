33ad0195ea283ea7127d143192afdfedd7d091ac
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-01 22:54:50 +0700

    fix: redirect registration to login with success message

--- app/Http/Responses/RegisterResponse.php
+++ app/Http/Responses/RegisterResponse.php
@@ -4,15 +4,16 @@ namespace App\Http\Responses;
 
 use Illuminate\Http\JsonResponse;
 use Laravel\Fortify\Contracts\RegisterResponse as RegisterResponseContract;
-use Laravel\Fortify\Fortify;
 use Symfony\Component\HttpFoundation\Response;
 
 class RegisterResponse implements RegisterResponseContract
 {
     public function toResponse($request): Response
     {
-        return $request->wantsJson()
-            ? new JsonResponse(['two_factor' => false], 201)
-            : redirect()->intended(Fortify::redirects('register'));
+        if ($request->wantsJson()) {
+            return new JsonResponse(['two_factor' => false], 201);
+        }
+
+        return redirect()->route('login')->with('status', 'registration-success');
     }
 }

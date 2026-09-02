7541c78f1674184e23cad4c5eab9e7c694fe7903
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-02 22:07:59 +0700

    Fix email verification redirect for TechStore

--- app/Http/Responses/VerifyEmailResponse.php
+++ app/Http/Responses/VerifyEmailResponse.php
@@ -2,25 +2,18 @@
 
 namespace App\Http\Responses;
 
-use App\Http\Responses\Concerns\RedirectsToCurrentTeam;
 use Illuminate\Http\JsonResponse;
 use Laravel\Fortify\Contracts\VerifyEmailResponse as VerifyEmailResponseContract;
-use Laravel\Fortify\Fortify;
 use Symfony\Component\HttpFoundation\Response;
 
 class VerifyEmailResponse implements VerifyEmailResponseContract
 {
-    use RedirectsToCurrentTeam;
-
     public function toResponse($request): Response
     {
         if ($request->wantsJson()) {
             return new JsonResponse('', 204);
         }
 
-        $team = $this->currentTeam($request);
-        $redirect = "/{$team->slug}".Fortify::redirects('email-verification');
-
-        return redirect()->intended($redirect.'?verified=1');
+        return redirect()->intended('/?verified=1');
     }
 }

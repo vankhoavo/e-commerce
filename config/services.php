d8505d236cdd2bbd85f5c3ea577bfbf16a334618
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-02 20:50:17 +0700

    Clean unused SMS service configuration

--- config/services.php
+++ config/services.php
@@ -37,14 +37,4 @@ return [
         'vnd_to_usd' => (float) env('PAYPAL_VND_TO_USD', 25000),
     ],
 
-    'sms' => [
-        'driver' => env('SMS_DRIVER', 'log'),
-    ],
-
-    'twilio' => [
-        'sid' => env('TWILIO_SID'),
-        'token' => env('TWILIO_AUTH_TOKEN'),
-        'from' => env('TWILIO_FROM'),
-    ],
-
 ];

b510964aacf4c65efcb53e4b191bad50084defe7
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-02 13:28:02 +0700

    Remove team setup from user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Enums\TeamRole;
 use App\Enums\UserRole;
-use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
@@ -33,21 +31,6 @@ class UserFactory extends Factory
             'avatar' => null,
             'google_id' => null,
         ];
-    }
-
-    public function configure(): static
-    {
-        return $this->afterCreating(function ($user) {
-            $team = Team::factory()->personal()->create([
-                'name' => $user->name."'s Team",
-            ]);
-
-            $team->members()->attach($user, [
-                'role' => TeamRole::Owner->value,
-            ]);
-
-            $user->switchTeam($team);
-        });
     }
 
     public function unverified(): static

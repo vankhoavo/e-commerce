da03248743d471cbd768173bb36781156cf078e9
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-03 19:04:56 +0700

    feat: register OTP verification middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\EnsureAdmin;
 use App\Http\Middleware\EnsureAdminPermission;
+use App\Http\Middleware\EnsureEmailOtpVerified;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
@@ -14,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(web: __DIR__.'/../routes/web.php',commands: __DIR__.'/../routes/console.php',health: '/up')
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance','sidebar_state']);
-        $middleware->alias(['admin'=>EnsureAdmin::class,'admin.permission'=>EnsureAdminPermission::class]);
+        $middleware->alias(['admin'=>EnsureAdmin::class,'admin.permission'=>EnsureAdminPermission::class,'otp.verified'=>EnsureEmailOtpVerified::class]);
         $middleware->web(append: [HandleAppearance::class,HandleInertiaRequests::class,AddLinkHeadersForPreloadedAssets::class]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {

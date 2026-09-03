b10c5c103f0cacf29bc4f654fb09a164a497aa27
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-03 21:16:30 +0700

    feat: add sales technical and customer service roles

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -1,10 +1,26 @@
 <?php
 
-namespace App\Enums;
-
 enum UserRole: string
 {
     case ADMIN = 'admin';
-    case STAFF = 'staff';
+    case SALES = 'sales';
+    case TECHNICAL = 'technical';
+    case CUSTOMER_SERVICE = 'customer_service';
     case CUSTOMER = 'customer';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::ADMIN => 'Quản trị viên',
+            self::SALES => 'Bán hàng',
+            self::TECHNICAL => 'Kỹ thuật',
+            self::CUSTOMER_SERVICE => 'Chăm sóc khách hàng',
+            self::CUSTOMER => 'Khách hàng',
+        };
+    }
+
+    public function isBackOffice(): bool
+    {
+        return in_array($this, [self::ADMIN, self::SALES, self::TECHNICAL, self::CUSTOMER_SERVICE], true);
+    }
 }

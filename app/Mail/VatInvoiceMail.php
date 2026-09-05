5c093537cfaca014646a6ff73cc9d5fffd326ecb
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-06 01:58:21 +0700

    Align VAT invoice mailable with Laravel Cloud default queue

--- app/Mail/VatInvoiceMail.php
+++ app/Mail/VatInvoiceMail.php
@@ -18,7 +18,7 @@ class VatInvoiceMail extends Mailable implements ShouldQueue
 
     public function __construct(public Order $order)
     {
-        $this->onQueue('emails');
+        $this->onQueue('default');
     }
 
     public function envelope(): Envelope

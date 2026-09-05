090b47f22acbdecfce4ba3a6a129afbd2557520c
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-06 01:35:09 +0700

    Queue VAT invoice mail on emails queue

--- app/Mail/VatInvoiceMail.php
+++ app/Mail/VatInvoiceMail.php
@@ -5,18 +5,20 @@ namespace App\Mail;
 use App\Models\Order;
 use App\Services\VatInvoicePdfService;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Queue\SerializesModels;
 
-class VatInvoiceMail extends Mailable
+class VatInvoiceMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
     public function __construct(public Order $order)
     {
+        $this->onQueue('emails');
     }
 
     public function envelope(): Envelope

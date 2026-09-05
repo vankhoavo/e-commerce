091aa500f0f70e2537c1482e7217eca4caaa0afa
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-06 02:06:21 +0700

    Fix queued email architecture for order notifications

--- app/Mail/OrderReceivedMail.php
+++ app/Mail/OrderReceivedMail.php
@@ -3,21 +3,13 @@
 namespace App\Mail;
 
 use App\Models\Order;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
 
-class OrderReceivedMail extends Mailable implements ShouldQueue
+class OrderReceivedMail extends Mailable
 {
-    use Queueable, SerializesModels;
-
-    public function __construct(public Order $order)
-    {
-        $this->onQueue('default');
-    }
+    public function __construct(public Order $order) {}
 
     public function envelope(): Envelope
     {

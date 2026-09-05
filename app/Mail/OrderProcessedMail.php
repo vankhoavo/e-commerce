35baac29c2d34c073c9dbaf8d2ee4abf03101ea6
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-06 01:35:03 +0700

    Queue order processed mail on emails queue

--- app/Mail/OrderProcessedMail.php
+++ app/Mail/OrderProcessedMail.php
@@ -4,17 +4,19 @@ namespace App\Mail;
 
 use App\Models\Order;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class OrderProcessedMail extends Mailable
+class OrderProcessedMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
     public function __construct(public Order $order)
     {
+        $this->onQueue('emails');
     }
 
     public function envelope(): Envelope
